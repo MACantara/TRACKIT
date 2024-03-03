1e75ac00863e6900f7b50ad6b3db00f191f641f2
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-03-03 20:00:42 +0800

    Add dependencies and remove unused code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, send_from_directory
+from flask import Flask, render_template, request, redirect, send_from_directory, make_response
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from psycopg2 import connect, Error
